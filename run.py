3c756bb673006faea0c9a31ae17b3db0150dbc03
Brandon Iffert <brandoniffert@gmail.com>
2015-07-08 15:55:03 -0400

    allow run.py to accept --debug flag

--- run.py
+++ run.py
@@ -1,4 +1,9 @@
+import argparse
 from app import app
 
+parser = argparse.ArgumentParser()
+parser.add_argument('--debug', action='store_true', default=False, dest='debug')
+args = parser.parse_args()
+
 if __name__ == '__main__':
-    app.run(threaded=True)
+    app.run(threaded=True, debug=args.debug)
