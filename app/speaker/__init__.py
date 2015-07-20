5b1a24401ba551abc055cfd15ceeaca3ed211e35
Brandon Iffert <brandoniffert@gmail.com>
2015-07-20 13:10:47 -0400

    slow down speaking

--- app/speaker/__init__.py
+++ app/speaker/__init__.py
@@ -2,4 +2,4 @@ import subprocess
 
 
 def say(message):
-    subprocess.check_call(['espeak', "{}".format(message)], stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT)
+    subprocess.check_call(['espeak', '-s', '140', "{}".format(message)], stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT)
